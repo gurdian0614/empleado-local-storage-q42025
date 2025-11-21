7ec2669a6d9687ac71075ee527430e87cd808ec5
gurdian0614 <ivan.gurdian@outlook.com>
2025-11-21 17:59:09 -0600

    Create component

---
+++ src/components/GestorEmpleado.tsx
@@ -0,0 +1,12 @@
+import { useEmpleado } from "../hooks/useEmpleado";
+
+const GestorEmpleado: React.FC = () => {
+
+    return (
+        <>
+            <h1>Gestor de Empleados</h1>
+        </>
+    );
+}
+
+export default GestorEmpleado;
