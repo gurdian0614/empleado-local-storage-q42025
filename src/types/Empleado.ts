9eec0b1f6707eb505a8fae2217a918bcb953d32a
gurdian0614 <ivan.gurdian@outlook.com>
2025-11-25 20:52:39 -0600

    Adding types

--- src/types/Empleado.ts
+++ src/types/Empleado.ts
@@ -3,10 +3,22 @@ export interface Empleado {
     nombre: string;
     cargo: string;
     departamento: string;
-}
+};
 
 export interface FormularioDatos {
     nombre: string;
     cargo: string;
     departamento: string;
+};
+
+export interface Props {
+    agregarActualizarEmpleado: (e: Empleado) => void;
+    empleadoEditar: Empleado | null;
+    setEmpleadoEditar: (e: Empleado | null) => void;
+};
+
+export interface ListaEmpleadoProps {
+    empleados: Empleado[];
+    setEmpleadoEditar: (empleado: Empleado) => void;
+    eliminarEmpleado: (id: string) => void;
 }
